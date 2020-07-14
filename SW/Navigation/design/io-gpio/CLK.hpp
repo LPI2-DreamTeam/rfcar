d753d5010e4fe59206f2cf6c0f3271987326292d
Jose Mendes <user.email=josepr.mendes@gmail.com>
2020-07-14 22:55:11 +0100

    ADD: GPIO unit test (not totally successful) and resulting changes

---
+++ SW/Navigation/design/io-gpio/CLK.hpp
@@ -0,0 +1,19 @@
+#ifndef _CLK_H
+#define _CLK_H
+
+
+#include <chrono>
+
+namespace CLK {
+
+	typedef std::chrono::time_point<std::chrono::system_clock> Time;
+	typedef std::chrono::milliseconds TimeUnit;
+
+	typedef void *(TimeElapsedCallback)(void*);
+
+	typedef enum Error_T {
+		OK=0, NOT_CONFIG, STILL_RUNNING
+	} Error;
+}
+
+#endif //_CLK_H
---
+++ SW/Navigation/design/io-gpio/OS_SharedMemory.cpp
@@ -0,0 +1,2 @@
+#include "OS_SharedMemory.hpp"
+
